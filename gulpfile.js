2175470008077d1049a81b3783c5a0e91361c38f
Dima.mihaylov <playcssv34@gmail.com>
2022-01-18 16:50:55 +0300

    changes gulpfile .create

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
